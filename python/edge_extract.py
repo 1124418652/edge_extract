88a697a6273746691662ccd9e7c5608f761982fe
hejun <1124418652@qq.com>
2018-09-07 12:02:03 +0800

    finish function OTSU()

--- python/edge_extract.py
+++ python/edge_extract.py
@@ -91,11 +91,61 @@ def get_contours(binary_img, thresold = 100, mod = "sobel"):
 
 		return res_img
 
+def OTSU(img_array):
+	height = img_array.shape[0]
+	width = img_array.shape[1]
+	count_pixel = np.zeros(256)
+
+	for i in range(height):
+		for j in range(width):
+			count_pixel[int(img_array[i][j])] += 1 
+
+	# fig = plt.figure()
+	# ax = fig.add_subplot(111)
+	# ax.bar(np.linspace(0, 255, 256), count_pixel)
+	# ax.set_xlabel("pixels")
+	# ax.set_ylabel("num")
+	# plt.show()
+
+	max_variance = 0.0
+	best_thresold = 0
+	for thresold in range(256):
+		n0 = count_pixel[:thresold].sum()
+		n1 = count_pixel[thresold:].sum()
+		w0 = n0 / (height * width)
+		w1 = n1 / (height * width)
+		u0 = 0.0
+		u1 = 0.0
+		
+		for i in range(thresold):
+			u0 += i * count_pixel[i]
+		for j in range(thresold, 256):
+			u1 += j * count_pixel[j]
+		
+		u = u0 * w0 + u1 * w1 
+		tmp_var = w0 * np.power((u - u0), 2) + w1 * np.power((u - u1), 2)
+
+		if tmp_var > max_variance:
+			best_thresold = thresold
+			max_variance = tmp_var
+
+	return best_thresold
+
 def main():
 	path = "211.jpg"
 	img = np.array(Image.open(path).convert("L"))
-	binary_im = change_to_binary(img, 125, 255)
+	print(OTSU(img))
+
+	start = time.time()
+	binary_im = change_to_binary(img, OTSU(img), 255, "inverse")
+	end1 = time.time()
+	print(end1 - start)
+
+	plt.imshow(Image.fromarray(binary_im))
+
 	res_img = get_contours(binary_im, 100, "sobel")
+	end2 = time.time()
+	print(end2 - end1)
 	res_img = Image.fromarray(res_img)
 	plt.imshow(res_img)
 	plt.show()
